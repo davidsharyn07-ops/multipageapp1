fb26621b2d6edf04b1c4c6fe246f6953bcf4d806
davidsharyn07-ops <davidsharyn07@gmail.com>
2026-04-25 22:01:53 +0800

    Update 🌻main.py

--- DavidSharyn_Portfolio_MultiPageApp/🌻main.py
+++ DavidSharyn_Portfolio_MultiPageApp/🌻main.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+Home = st.Page("Pages/1 👩🏻‍💻Home.py")
+About = st.Page("Pages/2 👩🏻‍💼About.py")
+SKills= st.Page("Pages/3 🤓Skills.py")
+Projects = st.Page("Pages/4 💻Projects.py")
+Contact = st.Page("Pages/5 💌Contact.py")
+
+pg =  st.navigation([ Home , About , SKills , Projects , Contact])
+pg.run()
+
 st.title("🌻This is my Portfolio")
 st.markdown("""
 Take an insight of my innovation hub!
@@ -11,12 +20,5 @@ guide side bar to know me more:
 - 💻Projects
 - 💌Contact
 """)
-Home = st.Page("Pages/1 👩🏻‍💻Home.py")
-About = st.Page("Pages/2 👩🏻‍💼About.py")
-SKills= st.Page("Pages/3 🤓Skills.py")
-Projects = st.Page("Pages/4 💻Projects.py")
-Contact = st.Page("Pages/5 💌Contact.py")
 
-pg =  st.navigation([ Home , About , SKills , Projects , Contact])
-pg.run()
 st.info("This app shows my skills and information about python and others.")
