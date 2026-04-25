7219b799031c6818f73c9f8e560dc64c8d0ac425
davidsharyn07-ops <davidsharyn07@gmail.com>
2026-04-25 22:07:31 +0800

    Update 🌻main.py

--- DavidSharyn_Portfolio_MultiPageApp/🌻main.py
+++ DavidSharyn_Portfolio_MultiPageApp/🌻main.py
@@ -1,11 +1,5 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="David Portfolio",
-    page_icon="🌻",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
 
 Home = st.Page("Pages/1 👩🏻‍💻Home.py")
 About = st.Page("Pages/2 👩🏻‍💼About.py")
@@ -16,16 +10,3 @@ Contact = st.Page("Pages/5 💌Contact.py")
 pg =  st.navigation([ Home , About , SKills , Projects , Contact])
 pg.run()
 
-st.title("🌻This is my Portfolio")
-st.markdown("""
-Take an insight of my innovation hub!
-
-guide side bar to know me more:
-- 👩🏻‍💻Home
-- 👩🏻‍💼About
-- 🤓SKills
-- 💻Projects
-- 💌Contact
-""")
-
-st.info("This app shows my skills and information about python and others.")
