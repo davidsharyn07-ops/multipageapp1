db1b6d90e45c0932505c5d30dbea3e9c386e21d3
davidsharyn07-ops <davidsharyn07@gmail.com>
2026-04-25 22:08:40 +0800

    Update 1 👩🏻‍💻Home.py

--- DavidSharyn_Portfolio_MultiPageApp/Pages/1 👩🏻‍💻Home.py
+++ DavidSharyn_Portfolio_MultiPageApp/Pages/1 👩🏻‍💻Home.py
@@ -1,6 +1,7 @@
 import streamlit as st 
 
 st.title("👩🏻‍💻Home")
+st.title("🌻This is my Portfolio")
 st.header("Wassup, It's Sharyn")
 st.title("💭 welcome to my innovation hub")
 st.write("Aspiring Developer | Designer | Innovator")
